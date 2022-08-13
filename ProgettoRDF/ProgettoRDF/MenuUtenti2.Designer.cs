89d5b3d485bd873ee8b16f1fbc53e379eae71f10
Abro01 <93769284+Abro01@users.noreply.github.com>
2022-08-13 13:52:29 +0200

    Design

---
+++ ProgettoRDF/ProgettoRDF/MenuUtenti2.Designer.cs
@@ -0,0 +1,273 @@
+﻿namespace ProgettoRDF
+{
+    partial class MenuUtenti2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MenuUtenti2));
+            this.pnlLogo = new System.Windows.Forms.Panel();
+            this.lblAP = new System.Windows.Forms.Label();
+            this.btnLogout = new System.Windows.Forms.Button();
+            this.btnImpostazioni = new System.Windows.Forms.Button();
+            this.btnProfilo = new System.Windows.Forms.Button();
+            this.btnCarrello = new System.Windows.Forms.Button();
+            this.btnRicerca = new System.Windows.Forms.Button();
+            this.btnHome = new System.Windows.Forms.Button();
+            this.pnlMenu = new System.Windows.Forms.Panel();
+            this.pnlTitolo = new System.Windows.Forms.Panel();
+            this.lblTitolo = new System.Windows.Forms.Label();
+            this.pnlDesktop = new System.Windows.Forms.Panel();
+            this.pnlLogo.SuspendLayout();
+            this.pnlMenu.SuspendLayout();
+            this.pnlTitolo.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlLogo
+            // 
+            this.pnlLogo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(39)))), ((int)(((byte)(58)))));
+            this.pnlLogo.Controls.Add(this.lblAP);
+            this.pnlLogo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlLogo.Location = new System.Drawing.Point(0, 0);
+            this.pnlLogo.Name = "pnlLogo";
+            this.pnlLogo.Size = new System.Drawing.Size(220, 80);
+            this.pnlLogo.TabIndex = 1;
+            // 
+            // lblAP
+            // 
+            this.lblAP.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblAP.AutoSize = true;
+            this.lblAP.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAP.ForeColor = System.Drawing.Color.White;
+            this.lblAP.Location = new System.Drawing.Point(55, 19);
+            this.lblAP.Name = "lblAP";
+            this.lblAP.Size = new System.Drawing.Size(88, 40);
+            this.lblAP.TabIndex = 0;
+            this.lblAP.Text = "AeP";
+            this.lblAP.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnLogout
+            // 
+            this.btnLogout.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnLogout.FlatAppearance.BorderSize = 0;
+            this.btnLogout.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogout.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogout.ForeColor = System.Drawing.Color.White;
+            this.btnLogout.Image = ((System.Drawing.Image)(resources.GetObject("btnLogout.Image")));
+            this.btnLogout.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnLogout.Location = new System.Drawing.Point(0, 380);
+            this.btnLogout.Name = "btnLogout";
+            this.btnLogout.Padding = new System.Windows.Forms.Padding(8, 0, 0, 0);
+            this.btnLogout.Size = new System.Drawing.Size(220, 60);
+            this.btnLogout.TabIndex = 7;
+            this.btnLogout.Text = "   Logout";
+            this.btnLogout.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnLogout.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnLogout.UseVisualStyleBackColor = true;
+            this.btnLogout.Click += new System.EventHandler(this.btnLogout_Click);
+            // 
+            // btnImpostazioni
+            // 
+            this.btnImpostazioni.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnImpostazioni.FlatAppearance.BorderSize = 0;
+            this.btnImpostazioni.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnImpostazioni.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnImpostazioni.ForeColor = System.Drawing.Color.White;
+            this.btnImpostazioni.Image = ((System.Drawing.Image)(resources.GetObject("btnImpostazioni.Image")));
+            this.btnImpostazioni.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnImpostazioni.Location = new System.Drawing.Point(0, 320);
+            this.btnImpostazioni.Name = "btnImpostazioni";
+            this.btnImpostazioni.Padding = new System.Windows.Forms.Padding(8, 0, 0, 0);
+            this.btnImpostazioni.Size = new System.Drawing.Size(220, 60);
+            this.btnImpostazioni.TabIndex = 6;
+            this.btnImpostazioni.Text = "   Impostazioni";
+            this.btnImpostazioni.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnImpostazioni.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnImpostazioni.UseVisualStyleBackColor = true;
+            this.btnImpostazioni.Click += new System.EventHandler(this.btnImpostazioni_Click);
+            // 
+            // btnProfilo
+            // 
+            this.btnProfilo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnProfilo.FlatAppearance.BorderSize = 0;
+            this.btnProfilo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnProfilo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnProfilo.ForeColor = System.Drawing.Color.White;
+            this.btnProfilo.Image = ((System.Drawing.Image)(resources.GetObject("btnProfilo.Image")));
+            this.btnProfilo.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnProfilo.Location = new System.Drawing.Point(0, 260);
+            this.btnProfilo.Name = "btnProfilo";
+            this.btnProfilo.Padding = new System.Windows.Forms.Padding(8, 0, 0, 0);
+            this.btnProfilo.Size = new System.Drawing.Size(220, 60);
+            this.btnProfilo.TabIndex = 5;
+            this.btnProfilo.Text = "   Il mio account";
+            this.btnProfilo.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnProfilo.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnProfilo.UseVisualStyleBackColor = true;
+            this.btnProfilo.Click += new System.EventHandler(this.btnProfilo_Click);
+            // 
+            // btnCarrello
+            // 
+            this.btnCarrello.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnCarrello.FlatAppearance.BorderSize = 0;
+            this.btnCarrello.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCarrello.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCarrello.ForeColor = System.Drawing.Color.White;
+            this.btnCarrello.Image = ((System.Drawing.Image)(resources.GetObject("btnCarrello.Image")));
+            this.btnCarrello.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCarrello.Location = new System.Drawing.Point(0, 200);
+            this.btnCarrello.Name = "btnCarrello";
+            this.btnCarrello.Padding = new System.Windows.Forms.Padding(8, 0, 0, 0);
+            this.btnCarrello.Size = new System.Drawing.Size(220, 60);
+            this.btnCarrello.TabIndex = 4;
+            this.btnCarrello.Text = "   Carrello";
+            this.btnCarrello.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCarrello.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnCarrello.UseVisualStyleBackColor = true;
+            this.btnCarrello.Click += new System.EventHandler(this.btnCarrello_Click);
+            // 
+            // btnRicerca
+            // 
+            this.btnRicerca.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnRicerca.FlatAppearance.BorderSize = 0;
+            this.btnRicerca.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRicerca.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRicerca.ForeColor = System.Drawing.Color.White;
+            this.btnRicerca.Image = ((System.Drawing.Image)(resources.GetObject("btnRicerca.Image")));
+            this.btnRicerca.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnRicerca.Location = new System.Drawing.Point(0, 140);
+            this.btnRicerca.Name = "btnRicerca";
+            this.btnRicerca.Padding = new System.Windows.Forms.Padding(8, 0, 0, 0);
+            this.btnRicerca.Size = new System.Drawing.Size(220, 60);
+            this.btnRicerca.TabIndex = 3;
+            this.btnRicerca.Text = "   Cerca";
+            this.btnRicerca.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnRicerca.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnRicerca.UseVisualStyleBackColor = true;
+            this.btnRicerca.Click += new System.EventHandler(this.btnRicerca_Click);
+            // 
+            // btnHome
+            // 
+            this.btnHome.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnHome.FlatAppearance.BorderSize = 0;
+            this.btnHome.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnHome.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHome.ForeColor = System.Drawing.Color.White;
+            this.btnHome.Image = ((System.Drawing.Image)(resources.GetObject("btnHome.Image")));
+            this.btnHome.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnHome.Location = new System.Drawing.Point(0, 80);
+            this.btnHome.Name = "btnHome";
+            this.btnHome.Padding = new System.Windows.Forms.Padding(8, 0, 0, 0);
+            this.btnHome.Size = new System.Drawing.Size(220, 60);
+            this.btnHome.TabIndex = 2;
+            this.btnHome.Text = "   Home";
+            this.btnHome.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnHome.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnHome.UseVisualStyleBackColor = true;
+            this.btnHome.Click += new System.EventHandler(this.btnHome_Click);
+            // 
+            // pnlMenu
+            // 
+            this.pnlMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(76)))));
+            this.pnlMenu.Controls.Add(this.btnLogout);
+            this.pnlMenu.Controls.Add(this.btnImpostazioni);
+            this.pnlMenu.Controls.Add(this.btnProfilo);
+            this.pnlMenu.Controls.Add(this.btnCarrello);
+            this.pnlMenu.Controls.Add(this.btnRicerca);
+            this.pnlMenu.Controls.Add(this.btnHome);
+            this.pnlMenu.Controls.Add(this.pnlLogo);
+            this.pnlMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnlMenu.Location = new System.Drawing.Point(0, 0);
+            this.pnlMenu.Name = "pnlMenu";
+            this.pnlMenu.Size = new System.Drawing.Size(220, 574);
+            this.pnlMenu.TabIndex = 1;
+            // 
+            // pnlTitolo
+            // 
+            this.pnlTitolo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(150)))), ((int)(((byte)(136)))));
+            this.pnlTitolo.Controls.Add(this.lblTitolo);
+            this.pnlTitolo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlTitolo.Location = new System.Drawing.Point(220, 0);
+            this.pnlTitolo.Name = "pnlTitolo";
+            this.pnlTitolo.Size = new System.Drawing.Size(1058, 80);
+            this.pnlTitolo.TabIndex = 2;
+            // 
+            // lblTitolo
+            // 
+            this.lblTitolo.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblTitolo.AutoSize = true;
+            this.lblTitolo.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitolo.ForeColor = System.Drawing.Color.White;
+            this.lblTitolo.Location = new System.Drawing.Point(483, 19);
+            this.lblTitolo.Name = "lblTitolo";
+            this.lblTitolo.Size = new System.Drawing.Size(129, 40);
+            this.lblTitolo.TabIndex = 0;
+            this.lblTitolo.Text = "HOME";
+            // 
+            // pnlDesktop
+            // 
+            this.pnlDesktop.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlDesktop.Location = new System.Drawing.Point(220, 80);
+            this.pnlDesktop.Name = "pnlDesktop";
+            this.pnlDesktop.Size = new System.Drawing.Size(1058, 494);
+            this.pnlDesktop.TabIndex = 3;
+            // 
+            // MenuUtenti2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1278, 574);
+            this.Controls.Add(this.pnlDesktop);
+            this.Controls.Add(this.pnlTitolo);
+            this.Controls.Add(this.pnlMenu);
+            this.Name = "MenuUtenti2";
+            this.Text = "MenuUtenti2";
+            this.Load += new System.EventHandler(this.MenuUtenti2_Load);
+            this.pnlLogo.ResumeLayout(false);
+            this.pnlLogo.PerformLayout();
+            this.pnlMenu.ResumeLayout(false);
+            this.pnlTitolo.ResumeLayout(false);
+            this.pnlTitolo.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlLogo;
+        private System.Windows.Forms.Label lblAP;
+        private System.Windows.Forms.Button btnLogout;
+        private System.Windows.Forms.Button btnImpostazioni;
+        private System.Windows.Forms.Button btnProfilo;
+        private System.Windows.Forms.Button btnCarrello;
+        private System.Windows.Forms.Button btnRicerca;
+        private System.Windows.Forms.Button btnHome;
+        private System.Windows.Forms.Panel pnlMenu;
+        private System.Windows.Forms.Panel pnlTitolo;
+        private System.Windows.Forms.Label lblTitolo;
+        private System.Windows.Forms.Panel pnlDesktop;
+    }
+}
---
+++ ProgettoRDF/ProgettoRDF/MenuUtenti2.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProgettoRDF
+{
+    public partial class MenuUtenti2 : Form
+    {
+        private Button currentButton;
+        private Random random;
+        private int tempIndex;
+        private Form activateForm;
+
+        myDBconnection con = new myDBconnection();
+
+        public MenuUtenti2()
+        {
+            InitializeComponent();
+            random = new Random();
+        }
+
+        private Color SelectThemeColor()
+        {
+            int index = random.Next(ThemeColor.ColorList.Count);
+
+            while (tempIndex == index)
+            {
+                index = random.Next(ThemeColor.ColorList.Count);
+            }
+
+            tempIndex = index;
+            string color = ThemeColor.ColorList[index];
+
+            return ColorTranslator.FromHtml(color);
+        }
+
+        private void ActivateButton(object btnSender)
+        {
+            if (btnSender != null)
+            {
+                if (currentButton != (Button)btnSender)
+                {
+                    DisableButton();
+                    Color color = SelectThemeColor();
+                    currentButton = (Button)btnSender;
+                    currentButton.BackColor = color;
+                    currentButton.ForeColor = Color.White;
+                    currentButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                    pnlTitolo.BackColor = color;
+                    pnlLogo.BackColor = ThemeColor.ChangeColorBrightness(color, -0.3);
+                }
+            }
+        }
+
+        private void DisableButton()
+        {
+            foreach (Control previousBtn in pnlMenu.Controls)
+            {
+                previousBtn.BackColor = Color.FromArgb(51, 51, 76);
+                previousBtn.ForeColor = Color.White;
+                previousBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+
+            }
+        }
+
+        private void OpenChildForm(Form childForm, object btnSender)
+        {
+            if(activateForm != null)
+            {
+                activateForm.Close();
+            }
+
+            ActivateButton(btnSender);
+            activateForm = childForm;
+        }
+
+        private void btnHome_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender);
+        }
+
+        private void btnRicerca_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender);
+        }
+
+        private void btnCarrello_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender);
+        }
+
+        private void btnProfilo_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender);
+        }
+
+        private void btnImpostazioni_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender);
+        }
+
+        private void btnLogout_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender);
+            Login logout = new Login();
+            logout.Show();
+            this.Hide();
+
+            con.cn.Close();
+        }
+
+        private void MenuUtenti2_Load(object sender, EventArgs e)
+        {
+            con.cn.Open();
+        }
+    }
+}
